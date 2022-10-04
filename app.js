bb552edecaf18579f929098f9dd24e86bc8d95ab
Cristian Liu <cliu@est.frba.utn.edu.ar>
2022-10-03 21:59:31 -0300

    Agregue register y cambie login

--- app.js
+++ app.js
@@ -16,3 +16,6 @@ app.get("/Producto", (req, res) =>{
 app.get("/login", (req, res) =>{
     res.sendFile(__dirname + "/views/login.html");
 })
+app.get("/register", (req, res) =>{
+    res.sendFile(__dirname + "/views/register.html");
+})
