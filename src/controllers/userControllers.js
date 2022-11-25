3543a9510c2f1a9294b64e3b2295aaed5dc493e6
SoloTrane1958 <talktomatyaboutit@gmail.com>
2022-11-25 20:27:43 -0300

    resolver

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,9 +1,9 @@
 const fs = require ('fs');
 const path = require ('path');
 
-const productsFilePath = path.join(__dirname, '../data/products.json');
+const usersFilePath = path.join(__dirname, '../data/users.json')
 
-const productsDB = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const usersDB = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8')); 
 
 const userControllers= {
     index: function(req,res, next){
@@ -11,6 +11,35 @@ const userControllers= {
             products: productsDB
         });
     },
+
+    create: function (req, res) {
+        res.render('register'); 
+    },
+
+    users: function (req,res){
+        res.render('users');
+    }, 
+
+    store: function (req, res) {
+        let newUser = {
+            id: Date.now(), 
+            firstname : req.body.firstname,
+            lastname : req.body.lastname,
+            username : req.body.username,
+            password : req.body.password,
+            email: req.body.email,
+            avatar: req.file.filename, 
+        }
+
+        usersDB.push(newUser); 
+
+        fs.writeFileSync(usersFilePath, JSON.stringify(usersDB, null, ' ')); 
+
+        res.redirect('/index');
+         
+    }
+
+    
     /* login: function (req,res){
         res.render('login');
     },
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,14 +1,41 @@
+// 1- Requiero express y el metodo router. 
 const express = require('express');
 const router = express.Router();
+const path = require ('path');
 
+
+// 2- Requiero mi archivo de controladores. 
 const userControllers = require('../controllers/userControllers');
+//requiero multer en el archivo 
+const multer= require('multer');
+
+//tengo que crear dos variables que invocan diferentes metodos que ofrece multer. 
+const storage= multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, path.join(__dirname, '../../public/Imagenes/users'))
+    },
+    filename: function(req,res, callback){
+        cb(null, file.filename + '-' + Date.now() + path.extname(file.originalname))
+    }
+})
+
+const upload= multer({
+    storage
+})
 
 router.get('/', userControllers.index);
 
-/* router.get('/register', userControllers.register); 
+router.get('/users', userControllers.users); 
 
-router.get("/login",userControllers.login); 
+// 3- necesito de rutas, una ruta que me resuelva el formulario (get).
+router.get('/register', userControllers.create)
+// 4- y otra ruta que me resuelva la insercion de ese usuario (post). 
 
-router.get("/carritodecompras", userControllers.carrito); */
+//paso el nombre del input por el middleware
+router.post('/', upload.single('imagen'), userControllers.store); 
+
+
+
+
 
 module.exports = router; 
